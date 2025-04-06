b1d8af2308ede896a9ae11fac1dcc82358a3431d
NbekWeb <nbekdev@gmail.com>
2025-04-06 17:43:38 +0500

    first commit

--- src/components/Friend.vue
+++ src/components/Friend.vue
@@ -7,6 +7,10 @@ const props = defineProps({
     required: true,
   },
 });
+
+function formatNumber(value) {
+  return value?.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+}
 </script>
 
 <template>
@@ -14,9 +18,15 @@ const props = defineProps({
     class="rounded-md bg-dark-200 h-14 text-base font-nova flex items-center justify-between px-3"
   >
     <div class="flex items-center gap-4">
+      
       <profile-icon class="text-2xl" />
-      <span class="font-semibold">@JononTon</span>
+      
+      <span class="font-semibold">{{
+        data?.username ? `@${data.username}` : data?.first_name
+      }}</span>
     </div>
-    <span class="flex py-1.5 px-4 btn-orange"> 1.000.000</span>
+    <span class="flex py-1.5 px-4 btn-orange">
+      {{ formatNumber(data.user_level?.[0]?.total_bit) }}</span
+    >
   </div>
 </template>
